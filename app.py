00724c45bc1713a92825b27aa05beeb8262fe44f
MathurTarun06 <66874532+MathurTarun06@users.noreply.github.com>
2022-02-10 19:42:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app.secret_key = os.urandom(24)
 # creating a connection object to connect with mysql database
 connection_obj=mysql.connector.connect(host="remotemysql.com",user="5tRN1vkakY",password="M86NPCF8Pl",database="5tRN1vkakY")
 
-# a variable to communicate with database
-cursor = connection_obj.cursor()
+# var to communicate with database
+cursor_obj = connection_obj.cursor()
 
 @app.route("/")
 def helloworld():
@@ -22,7 +22,7 @@ def helloworld():
 # decorator for user's index after login
 @app.route('/index')
 def index():
-    # if user has logged in (i.e., session holds a key)then send it to user's index
+    # if user has key then login to index page
     if 'id' in session:
         return render_template('index.html')
     else:
@@ -33,13 +33,13 @@ def login_validation():
     email= request.form.get('email')
     password = request.form.get('password')
 
-    #query the database to check if email exists
-    cursor.execute("""SELECT * FROM `users` WHERE `email` LIKE '{}' AND `password` LIKE '{}'"""
+    #query  to check if email exists
+    cursor_obj.execute("""SELECT * FROM `users` WHERE `email` LIKE '{}' AND `password` LIKE '{}'"""
     .format(email,password))
     #store the result from above query in users
-    users= cursor.fetchall()
+    users = cursor_obj.fetchall()
 
-    #if user is found in db then redirect user to user's indexpage
+    #if user is found , redirect user to user's indexpage
     if len(users)>0:
         session['id']=users[0][0]
 
@@ -57,12 +57,12 @@ def add_user():
     password = request.form.get('upassword')
 
     # store the registered user data into db
-    cursor.execute("""INSERT INTO `users` (`id`,`name`, `email`, `password`) VALUES 
+    cursor_obj.execute("""INSERT INTO `users` (`id`,`name`, `email`, `password`) VALUES 
     (NULL,'{}','{}','{}')""".format(name,email,password))
     connection_obj.commit()
 
-    cursor.execute("""SELECT * FROM `users` WHERE `email` LIKE '{}'""".format(email))
-    myuser = cursor.fetchall()
+    cursor_obj.execute("""SELECT * FROM `users` WHERE `email` LIKE '{}'""".format(email))
+    myuser = cursor_obj.fetchall()
     session['id'] = myuser[0][0]
     return redirect('/index')
 
